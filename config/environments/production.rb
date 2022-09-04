b142195896d05c5dc1381b2cea2b0b3f5b781fb8
Samay Abid <99419808+SamayAbid@users.noreply.github.com>
2022-09-04 15:24:43 +0500

    add config action-mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,13 +119,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = {
-<<<<<<< HEAD
-  :host => 'afternoon-tor-92225.herokuapp.com'
-}
-=======
     :host => 'www.afternoon-tor-92225.herokuapp.com'
   }
-
-  
->>>>>>> sendgrid_branch
 end
